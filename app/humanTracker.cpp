ee1fb1437049bf4303c46fac7bca1433cf571c2e
Dhanush Babu Allam <dhanu0412@gmail.com>
2022-10-19 19:04:45 -0400

    1.3.3 write stubs for image processing

---
+++ app/humanTracker.cpp
@@ -0,0 +1,14 @@
+#include "../include/humanTracker.hpp"
+
+void HumanTracker::getCameraFrame(){
+    float averageHumanHeight = 172; 
+    //calculating the depth information (z coordinate)
+
+}
+void HumanTracker::transformRobotCoordinates(){
+
+}
+void HumanTracker::exitProgram(){
+    exit(0);
+}
+
