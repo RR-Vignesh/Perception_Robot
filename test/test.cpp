5cb680e72f63381f5931cb846c7c3f8471bebf93
Dhanush Babu Allam <dhanu0412@gmail.com>
2022-10-19 19:02:01 -0400

    1.3.2 create unit test for detector module

--- test/test.cpp
+++ test/test.cpp
@@ -9,3 +9,14 @@ TEST(getImage, should_pass) {
   Camera C;
   EXPECT_EQ(C.getImage().channels(), 3);
 }
+TEST(ImageHeightCheck, should_pass) {
+  Detector D;
+  cv::Mat image = D.getImage();
+  EXPECT_EQ(D.preProcessing(image).size().height, 640);
+}
+
+TEST(ImageWidthCheck, should_pass) {
+  Detector D;
+  cv::Mat image = D.getImage();
+  EXPECT_EQ(D.preProcessing(image).size().width, 640);
+}
